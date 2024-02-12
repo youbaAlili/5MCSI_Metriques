8a5a75ea87909dba8e963da29e7555fa0c8933f4
youbaAlili <59730737+youbaAlili@users.noreply.github.com>
2024-02-12 18:32:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return render_template("contact.html")
 
 @app.route("/rapport/")
 def mongraphique():
