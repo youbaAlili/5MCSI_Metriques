17247c0b059441d09e7467ec097eb4bba6f4d9a5
youbaAlili <59730737+youbaAlili@users.noreply.github.com>
2024-02-12 18:16:16 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ def MaPremiereAPI():
 def mongraphique():
     return render_template("graphique.html")
 
-@app.route("/paris/")
+@app.route("/histogramme/")
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
